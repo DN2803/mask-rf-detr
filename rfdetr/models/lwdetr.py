bd3d11f2e680aeada530c7c71dd622849b2c3632
dn28203 <ntkngann2k3@gmail.com>
2025-07-15 16:53:06 +0700

    fix

--- rfdetr/models/lwdetr.py
+++ rfdetr/models/lwdetr.py
@@ -681,7 +681,7 @@ class PostProcess(nn.Module):
                           For evaluation, this must be the original image size (before any data augmentation)
                           For visualization, this should be the image size after data augment, but before padding
         """
-        out_logits, out_bbox, out_mask = outputs['pred_logits'], outputs['pred_boxes']
+        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
 
         assert len(out_logits) == len(target_sizes)
         assert target_sizes.shape[1] == 2
